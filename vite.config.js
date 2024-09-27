2b6b1af90e13ac1ab50d148baa8a1a1cbce27745
“sid” <wangsester@gmail.com>
2024-09-27 12:36:56 +0800

    router更新V6

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: import.meta.env.MODE === 'production' ? '/your-repo-name/' : '/',
+  base: '/Unixecure_Web_2024_Vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
